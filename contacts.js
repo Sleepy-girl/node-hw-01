018ca1ea8b47b76f1ed5cb6320c2603813dac6f9
nyuta <pillowforneedles@gmail.com>
2020-10-02 18:32:54 +0300

    is not a finish

--- contacts.js
+++ contacts.js
@@ -1,37 +1,74 @@
-const fs = require("fs");
-// console.log(fs);
-
+const fs = require("fs").promises;
 const path = require("path");
+// console.log(fs);
 // console.log(path);
 
-const contactsPath = require("./db/contacts.json");
+// const contactsPath = require("./db/contacts.json");
 // console.log(contactsPath);
 
-function listContacts() {
-  const contacts = contactsPath.map((contact) => contact);
-  return contacts;
-}
-console.log(listContacts());
+class Contacts {
+  constructor() {
+    this.contactsPath = path.resolve(__dirname, "db", "contacts.json");
+  }
 
-function getContactById(contactId) {
-  return contactsPath.find((contact) => contact.id === contactId);
-}
-console.log(getContactById(9));
-
-function removeContact(contactId) {
-  return contactsPath.filter((contact) => contact.id !== contactId);
-}
-console.log(removeContact(10));
-
-function addContact(name, email, phone) {
-  const contact = {
-    id: contactsPath.length ? [...contactsPath].pop().id + 1 : 1,
-    name,
-    email,
-    phone,
+  listContacts = async () => {
+    const contactsData = await fs.readFile(this.contactsPath, {
+      encoding: "utf-8",
+    });
+    return JSON.parse(contactsData);
   };
-  return contactsPath.push(contact);
-}
-console.log(addContact("Anna", "pillow@gmail.com", "(050)545-0652"));
 
-module.exports = listContacts();
+  getContactById = async (contactId) => {
+    const contactsData = await this.listContacts();
+    return contactsData.find((contact) => contact.id === contactId);
+  };
+
+  removeContact = async (contactId) => {
+    const contactsData = await this.listContacts();
+    const result = contactsData.filter((contact) => contact.id !== contactId);
+    fs.writeFile(this.contactsPath, JSON.stringify(result));
+  };
+
+  addContact = async (name, email, phone) => {
+    const contactsData = await this.listContacts();
+    const newContact = {
+      id: contactsPath.length ? [...contactsData].pop().id + 1 : 1,
+      name,
+      email,
+      phone,
+    };
+    contactsPath.push(contact);
+    const contactsDataAsJSON = JSON.stringify(contactsData);
+    fs.writeFile(this.contactsPath, contactsDataAsJSON);
+    return newContact;
+  };
+}
+
+module.exports = new Contacts();
+
+//----------------------------------------------
+// const contactsPath = path.resolve(__dirname, "data", "contacts.json");
+// console.table(listContacts());
+
+// function getContactById(contactId) {
+//   return contactsPath.find((contact) => contact.id === contactId);
+// }
+// console.log(getContactById(9));
+
+// function removeContact(contactId) {
+//   return contactsPath.filter((contact) => contact.id !== contactId);
+// }
+// console.log(removeContact(10));
+
+// function addContact(name, email, phone) {
+//   const contact = {
+//     id: contactsPath.length ? [...contactsPath].pop().id + 1 : 1,
+//     name,
+//     email,
+//     phone,
+//   };
+//   return contactsPath.push(contact);
+// }
+// console.log(addContact("Anna", "pillow@gmail.com", "(050)545-0652"));
+
+// module.exports = { listContacts };
--- index.js
+++ index.js
@@ -6,5 +6,32 @@
 // const contactsPath = require("./db/contacts.json");
 // console.log(contactsPath);
 
-const contacts = require("./contacts");
-console.log(contacts);
+const Contacts = require("./contacts");
+// console.log(contacts);
+
+const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.table(await Contacts.listContacts());
+      break;
+
+    case "get":
+      console.table(await Contacts.getContactById(id));
+      break;
+
+    case "add":
+      console.table(await Contacts.addContact(name, email, phone));
+      break;
+
+    case "remove":
+      console.table(await Contacts.removeContact(9));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
